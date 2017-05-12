503f837b7922f87bc31f7b5ceacea18294bc37f2
Finn Masurat <finn.masurat@hotmail.de>
2017-05-12 15:23:53 +0200

    comments

--- aufgabe2.pl
+++ aufgabe2.pl
@@ -1,20 +1,23 @@
 :- use_module(library(clpfd)).
 
 sudoku(Rows) :-
-        append(Rows, Vs), Vs ins 1..9,
-        maplist(all_different, Rows),
+        append(Rows, Vs),                   % Konkatination von Rows in Vs. True wenn Rows eine Liste von Vs ist
+        Vs ins 1..9,                        % Vs sind Elemente von von 1..9
+        maplist(all_different, Rows),       % True wenn all_different auf Rows angewendet werden kann
         transpose(Rows, Columns),
-        maplist(all_different, Columns),
-        Rows = [A,B,C,D,E,F,G,H,I],
-        blocks(A, B, C), blocks(D, E, F), blocks(G, H, I),
-        label(Vs).
+        maplist(all_different, Columns),    % Transponiert Matrix für den Aufbau
+        Rows = [A,B,C,D,E,F,G,H,I],         % Rows mit Variablen
+        blocks(A, B, C),                    %
+        blocks(D, E, F),                    %
+        blocks(G, H, I),                    %
+        label(Vs).                          % Labeling der Liste
 
 blocks([], [], []).
 blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
         all_different([A,B,C,D,E,F,G,H,I]),
         blocks(Bs1, Bs2, Bs3).
 
-problem([[6,_,_,7,_,_,5,_,_],
+problem([[6,_,_,7,_,_,5,_,_],               % Problem das zu lösen ist
       [_,2,8,_,_,_,_,_,_],
       [_,_,_,6,4,_,3,_,_],
       [7,4,_,_,_,_,_,2,_],
