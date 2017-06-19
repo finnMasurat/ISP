d56526658cd4ce7d5eb95c2e60c96e9b17819ff2
Finn Masurat <finn.masurat@hotmail.de>
2017-06-19 23:44:39 +0200

    aufgabe 4 wip

---
+++ aufgabe4/eins.py
@@ -0,0 +1,13 @@
+import keras
+import tensorflow as tf
+from keras.models import Sequential
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+
+# Laden des Datensatzes und Aufteilung in 60.000 Trainings- und 10.000 Testdaten
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+example_idx = 50
+plt.title("Label: {}".format(y_train[example_idx]))
+plt.imshow(x_train[example_idx], cmap='gray')
+plt.show()
