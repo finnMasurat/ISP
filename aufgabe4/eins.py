ee4e92ff69eafe0586ee681222f84da3ae516122
Finn Masurat <finn.masurat@hotmail.de>
2017-06-21 12:47:54 +0200

    zweipunkteins.py added

--- aufgabe4/eins.py
+++ aufgabe4/eins.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 # Laden des Datensatzes und Aufteilung in 60.000 Trainings- und 10.000 Testdaten
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-example_idx = 50
+example_idx = 0
 plt.title("Label: {}".format(y_train[example_idx]))
 plt.imshow(x_train[example_idx], cmap='gray')
 plt.show()
